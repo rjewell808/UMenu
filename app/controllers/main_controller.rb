5793e5c55aeb2dc5a6ea007cc390e8a646404b3d
LITTLE-BERTHA <robertjewell808@gmail.com>
2019-10-19 18:34:31 -0400

    Fix parsing error; Update food page;

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -6,11 +6,13 @@ class MainController < ApplicationController
 		# for x in 0..45
 		# 	API.logDay(Date.today - x)
 		# end
+		#API.logDay(Date.today - 3)
 
 		@foods = Dish.pluck(:name).uniq
 	end
 
 	def food
 		@targets = Dish.where(name: params[:q]).order(:date)
+		@foodName = params[:q]
 	end
 end
--- app/models/api.rb
+++ app/models/api.rb
@@ -8,20 +8,27 @@ class API
 		data = JSON.parse res.body
 
 		meals = []
+		keyword = "data-dish-name=\""
 		data.each do |meal, stations|
+			if meal == "grabngo"
+				next
+			end
 			stations.each do |station, data|
 				lunch = data.to_s
-				while lunch.include? "data-dish-name="
-					keyword = "data-dish-name=\""
-					ind = lunch.index(keyword)
-					lunch = lunch[(ind+keyword.length)..-1]
-					indEnd = lunch.index("\"")
+				while lunch.include? keyword
+					indStart = lunch.index(keyword) + keyword.length
+					indEnd = lunch.index("\"", indStart) - 1
 
-					mealName = lunch[0..indEnd-1]
-					if Dish.where(name: mealName, meal: meal, date: date).size < 1
-						Dish.create(name: mealName, meal: meal, date: date, day: date.strftime("%A").downcase)
+					mealName = lunch[indStart..indEnd]
+
+					if !mealName.blank?
+						if Dish.where(name: mealName, meal: meal, date: date).size < 1
+							Dish.create(name: mealName, meal: meal, date: date, day: date.strftime("%A").downcase)
+						end
+						meals.append(mealName)
 					end
-					meals.append(mealName)
+
+					lunch = lunch[indEnd..-1]
 				end
 			end
 		end

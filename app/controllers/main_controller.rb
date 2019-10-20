612b25a828e90386cb73039c8dd400a1fd707cc7
Robert Jewell <robertjewell808@gmail.com>
2019-10-20 10:03:42 -0400

    Add more predictors

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -32,21 +32,7 @@ class MainController < ApplicationController
 			end
 			@last_day = date
 		end
-
-		@avg_cycle = @frequencies.sum / @frequencies.size.to_f
-		@next_serving = -1
-
 		@today = Time.zone.today
-
-		if @avg_cycle % 7 == 0
-			@next_serving = (@today + (@avg_cycle - (@today - @last_day)))
-		elsif @avg_cycle == 1
-			@next_serving = @today + 1
-		end
-			
-		if @next_serving != -1
-			@next_serving_days = (@next_serving - @today).to_i
-		end
 
 		@days_weights = []
 		start_date = @today
@@ -55,5 +41,40 @@ class MainController < ApplicationController
 			weeks = (end_date..start_date).to_a.select {|k| k.wday == ind }.size
 			@days_weights.append(weeks == 0 ? 0 : ((uniq_dates.select {|d| d.wday == ind }.size / weeks.to_f) * 100).to_i)
 	 	end
+
+		@avg_cycle = @frequencies.sum / @frequencies.size.to_f
+		@next_serving = -1
+
+		puts @days_weights
+
+		if @avg_cycle % 7 == 0
+			@next_serving = (@today + (@avg_cycle - (@today - @last_day)))
+		elsif @avg_cycle == 1
+			@next_serving = @today + 1
+		elsif @days_weights.include? 100
+			puts "yes"
+			weight_index = @last_day.wday
+			total = 0
+
+			while total < 7
+				if @days_weights[weight_index] == 100
+					@next_serving = @last_day + weight_index
+					break
+				else
+					weight_index += 1
+
+					if weight_index > 6
+						weight_index = 0
+					end
+					total += 1
+				end
+			end
+
+		end
+			
+		if @next_serving != -1
+			@next_serving_days = (@next_serving - @today).to_i
+		end
+
 	end
 end

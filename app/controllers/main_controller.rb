b261074907e7f5af976e4a1dcac10e9c6efd94d3
LITTLE-BERTHA <robertjewell808@gmail.com>
2019-10-20 00:42:52 -0400

    Clean up food page; Add more stats;

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -3,12 +3,12 @@ require 'set'
 
 class MainController < ApplicationController
 	def home
-		Dish.delete_all
-		Food.delete_all
+		# Dish.delete_all
+		# Food.delete_all
 		# for x in 0..45
 		# 	API.logDay(Time.zone.today - x)
 		# end
-		API.logDay(Date.today)
+		# API.logDay(Date.today)
 
 		@foods = Food.pluck(:name).uniq
 	end
